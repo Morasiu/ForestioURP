ce884f343c1bda86de9cf508f3c81c808239d78f
Hubert Morawski <morasiu2@gmail.com>
2020-02-01 17:08:02 +0100

    Done

---
+++ Assets/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        transform.LookAt(Vector3.zero, Vector3.forward);
+        transform.Rotate(new Vector3(-90, 0, 0), Space.Self);
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
