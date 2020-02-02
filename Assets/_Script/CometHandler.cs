8e1706092fa8c9db23ac43b87bcfe911de41c4ac
DawidSieczka <dawid.sieczka@pirum-technology.com>
2020-02-02 12:36:51 +0100

    komety

---
+++ Assets/_Script/CometHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometHandler : MonoBehaviour
+{
+    public GameObject CometPrefab;
+    public void SpawnComet()
+    {
+        Instantiate(CometPrefab, transform, false);
+    }
+}
